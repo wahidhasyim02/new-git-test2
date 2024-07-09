11a6c452793bbe9c698998c8cc7b66cb02be1eb8
Weha <88809550+weha46@users.noreply.github.com>
2024-07-09 11:48:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
+     <link rel="stylesheet" href="./src/style.css" />
 </head>
 <body>
     <h1>Hellow World!</h1>
