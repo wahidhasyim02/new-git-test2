e4f25e42cb9ce5db225097fba3942914abc33ca7
wahidhasyim02 <wahidhasyim46@gmail.com>
2024-07-09 11:56:47 +0700

    menambahkan list, font, script.js

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
     <link rel="stylesheet" href="./src/style.css" />
 </head>
 <body>
-    <h1>Hellow World!</h1>
+    <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem accusantium deleniti possimus aliquam ipsa ipsam alias, amet nisi voluptate iure natus sunt vitae. Odio, alias cupiditate iste quae harum quos.</p>
 <ul>
     <li>Satu</li>
     <li>Dua</li>
+    <li>Tiga</li>
+    <li>Empat</li>
 </ul>
 
 </body>
